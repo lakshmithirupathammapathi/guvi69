708faa1cb61cf92c579ab6ebb2572dd4bea8e282
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-08-22 11:40:37 +0530

    Create guvi69.java

---
+++ guvi69.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n1,n2,n;
+		Scanner sc=new Scanner(System.in);
+		n1=sc.nextInt();
+		n2=sc.nextInt();
+		n=n1-n2;
+		if(n%2==0)
+		{
+			System.out.print("even");
+		}
+		else
+		{
+			System.out.print("no");
+			
+		}
+		}
+	}
